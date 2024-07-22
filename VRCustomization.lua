f3ad82c31b7f6e0386de150bd44af23d8973d706
hadi <81989873+loadlua@users.noreply.github.com>
2024-07-21 20:38:05 -0400

    Update VRCustomization.lua

--- VRCustomization.lua
+++ VRCustomization.lua
@@ -3,5 +3,5 @@ skyvrversion = '2.0.0'
 local VR_Model_Customization_GUI = game:GetObjects("rbxassetid://18596045046")[1]
 VR_Model_Customization_GUI.Parent = game.CoreGui
 
-local MainFunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/loadlua/skyvr/main/VRCustomizationMain.lua"))()
-coroutine.wrap(MainFunc)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/loadlua/skyvr/main/VRCustomizationMain.lua"))()
+coroutine.wrap(fakescript)()
