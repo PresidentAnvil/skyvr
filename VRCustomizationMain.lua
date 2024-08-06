7d089d6742847bd89204a85ede0775ddf46bcb35
hadi <81989873+loadlua@users.noreply.github.com>
2024-08-06 16:13:28 -0400

    Export rewrite

--- VRCustomizationMain.lua
+++ VRCustomizationMain.lua
@@ -1,6 +1,4 @@
 function fakescript()
-	local script = Instance.new('LocalScript', VR_Model_Customization_GUI)
-
 	local global = getgenv()
 	global.skyvrsettings = {
 		headhats = {},
@@ -8,13 +6,14 @@ function fakescript()
 		rightarm = "",
 		toyhats = {leftarm = "",rightarm = ""}
 	}
-
+	
 	local Player = game:GetService("Players").LocalPlayer
 	local Character = Player.Character
 	--------------------------------------------
 	local MainFrame = script.Parent.MainFrame
 	local Settings = script.Parent.Settings
 	local ExtraSettings = script.Parent.ExtraSettings
+	local Export = script.Parent.Export
 	local Preview = script.Parent.PreviewFrame.Viewport
 	--------------------------------------------
 	local Tabs = MainFrame.Tabs.ScrollingFrame
@@ -30,7 +29,7 @@ function fakescript()
 	local rarmcf = Vector3.new(0,0,0)
 	local larmcf = Vector3.new(0,0,0)
 	local copy = toclipboard or setclipboard
-
+	
 	function alert(title,desc,dur)
 		game:GetService("StarterGui"):SetCore("SendNotification", {
 			Title = title;
@@ -47,13 +46,13 @@ function fakescript()
 		end
 		return false
 	end
-
+	
 	function findMeshID(char,id)
 		id = string.match(id,"%d+")
 		for i,v in char:GetChildren() do
 			if v:IsA("Accessory") then
 				local handle = v.Handle
-
+	
 				if handle:IsA("MeshPart") then
 					local pooped = string.match(handle.MeshId,"%d+")
 					if handle.MeshId:find('assetdelivery') then
@@ -74,10 +73,10 @@ function fakescript()
 				end
 			end
 		end
-
+	
 		return nil
 	end
-
+	
 	function dump(o)
 		if typeof(o) == 'table' then
 			local s = '{ '
@@ -102,29 +101,27 @@ function fakescript()
 			return tostring(o)
 		end
 	end
-
+	
 	if not Character then
 		Player.CharacterAdded:Wait()
-		Player.Character = Player.Character
+		Character = Player.Character
 	end
-
-	alert("Disclaimer","This GUI is intended to be ran on Solara and Wave, but the VR script will only work on Celery and Wave.",999)
 	
 	function updateList()
 		for i,v in ipairs(Selection:GetChildren()) do if v.Name ~= "Temp" and v:IsA("Frame") then v:Destroy() end end
-
+	
 		for i,v in ipairs(Character:GetChildren()) do
 			if not v:IsA("Accessory") then continue end
 			local Handle = v.Handle:Clone()
 			local newButton = tempHat:Clone()
 			local MeshId
-
+	
 			if Handle:IsA("MeshPart") then
 				MeshId = Handle.MeshId
 			else
 				MeshId = Handle:FindFirstChildOfClass("SpecialMesh").MeshId
 			end
-
+	
 			--local isUsed = table.find(global.skyvrsettings.headhats, 'meshid:'..string.match(MeshId,"%d+")) and table.find(global.skyvrsettings, 'meshid:'..string.match(MeshId,"%d+")) and table.find(global.skyvrsettings.toyhats, 'meshid:'..string.match(MeshId,"%d+"))
 			--print(isUsed)
 			if string.find(MeshId,'assetdelivery') then
@@ -189,7 +186,7 @@ function fakescript()
 					end
 				end)
 			end
-
+	
 			if currentPage == "armhats" then
 				if ifind(global.skyvrsettings.headhats, 'meshid:'..MeshId) then
 					newButton.Error.TextLabel.Text = "Already used in: Head Hats"
@@ -229,14 +226,14 @@ function fakescript()
 				else
 					newButton.Settings.BackgroundColor3 = default
 				end
-
+	
 				if global.skyvrsettings.toyhats.leftarm == 'meshid:'..MeshId then
 					newButton.Settings2.BackgroundColor3 = on
 				else
 					newButton.Settings2.BackgroundColor3 = default
 				end
 			end
-
+	
 			newButton.Settings.MouseButton1Click:Connect(function()
 				if newButton.Error.Visible == true then return end
 				if currentPage == "headhats" then
@@ -247,7 +244,7 @@ function fakescript()
 					if ifind(global.skyvrsettings.headhats, 'meshid:'..MeshId) then
 						local cf = global.skyvrsettings.headhats['meshid:'..MeshId]
 						local X,Y,Z = cf:ToOrientation()
-
+	
 						Settings.Selection.label.toggle.BackgroundColor3 = Color3.fromRGB(0, 233, 19)
 						Settings.Selection.hide.Visible = false
 						Settings.Selection.cfpos.Text = s(cf.X)..','..s(cf.Y)..','..s(cf.Z)
@@ -292,11 +289,11 @@ function fakescript()
 					end
 				end
 			end)
-
+	
 			newButton.Visible = true
 		end
 	end
-
+	
 	for i,v in ipairs(Tabs:GetChildren()) do
 		if v:IsA("ImageButton") then
 			v.MouseButton1Click:Connect(function()
@@ -305,22 +302,26 @@ function fakescript()
 			end)
 		end
 	end
-
+	
+	Export.x.MouseButton1Click:Connect(function()
+		Export.Visible = false
+	end)
+	
 	Settings.x.MouseButton1Click:Connect(function()
 		Settings.Visible = false
 		MainFrame.Visible = true
 	end)
-
+	
 	ExtraSettings.x.MouseButton1Click:Connect(function()
 		ExtraSettings.Visible = false
 		MainFrame.Visible = true
 	end)
-
+	
 	MainFrame.Settings.MouseButton1Click:Connect(function()
 		ExtraSettings.Visible = true
 		MainFrame.Visible = false
 	end)
-
+	
 	Settings.Selection.label.toggle.MouseButton1Click:Connect(function()
 		local enabled = Settings.Selection.label.toggle.BackgroundColor3 == Color3.fromRGB(0, 233, 19)
 		local MeshId = Settings.meshid.Text
@@ -332,14 +333,14 @@ function fakescript()
 			Settings.Selection.hide.Visible = true
 		else
 			global.skyvrsettings.headhats['meshid:'..MeshId] = CFrame.new(0,0,0)
-
+	
 			Settings.Selection.label.toggle.BackgroundColor3 = Color3.fromRGB(0, 233, 19)
 			Settings.Selection.hide.Visible = false
 			Settings.Selection.cfpos.Text = '0,0,0'
 			Settings.Selection.cfrot.Text = '0,0,0'
 		end
 	end)
-
+	
 	Settings.Selection.cfpos.FocusLost:Connect(function()
 		local split = string.split(Settings.Selection.cfpos.Text,',')
 		if #split == 3 and Settings.Selection.label.toggle.BackgroundColor3 == Color3.fromRGB(0, 233, 19) then
@@ -349,7 +350,7 @@ function fakescript()
 			Settings.Selection.cfpos.Text = '0,0,0'
 		end
 	end)
-
+	
 	Settings.Selection.cfrot.FocusLost:Connect(function()
 		local split = string.split(Settings.Selection.cfrot.Text,',')
 		local cf = global.skyvrsettings.headhats['meshid:'..Settings.meshid.Text]
@@ -360,20 +361,22 @@ function fakescript()
 			Settings.Selection.cfrot.Text = '0,0,0'
 		end
 	end)
-
+	
 	MainFrame.Export.MouseButton1Click:Connect(function()
 		-- let's convert this to a useable table in the script
 		local configs = global.skyvrsettings
 		local headhats = configs.headhats
 		local ldonething = 'Vector3.new('..s(larmcf)..')'
 		local rdonething = 'Vector3.new('..s(rarmcf)..')'
-		headhats = 'getgenv().headhats = '..dump(headhats)..'\ngetgenv().right = "'..configs.rightarm..'"\ngetgenv().left = "'..configs.leftarm..'"\ngetgenv().options = {\noutlinesEnabled = false, -- buggy-ish\nrighthandrotoffset = '..rdonething..', -- rotation is fit for the phantom right hand accessory\nlefthandrotoffset = '..ldonething..', -- rotation is fit for the phantom left hand accessory\nheadscale = 3,\nNetVelocity = Vector3.new(20,20,20), -- if your hands and head keep falling set these to higher numbers\ncontrollerRotationOffset = Vector3.new(90,90,0), -- movement offset\nHeadHatTransparency = 1,\nthirdPersonButtonToggle = nil, -- see what you look like (EXPERIMENTAL, set it to Enum.KeyCode.ButtonA if you want)\nleftToyBind = Enum.KeyCode.ButtonY, -- :D\nrightToyBind = Enum.KeyCode.ButtonB, -- :D\nleftToy = "'..configs.toyhats.leftarm..'", -- default is "" or nil\nrightToy = "'..configs.toyhats.rightarm..'", -- default is "" or nil\n}\ngetgenv().skyVRversion = \''..skyvrversion..'\'\nloadstring(game:HttpGet(\'https://raw.githubusercontent.com/loadlua/skyvr/main/SkyVR.lua\'))();'
-
-		print(headhats)
-		copy(headhats)
+	
+		local generatedScript = 'getgenv().headhats = '..dump(headhats)..'\ngetgenv().right = "'..configs.rightarm..'"\ngetgenv().left = "'..configs.leftarm..'"\ngetgenv().options = {\noutlinesEnabled = false, -- buggy-ish\nrighthandrotoffset = '..rdonething..', -- rotation is fit for the phantom right hand accessory\nlefthandrotoffset = '..ldonething..', -- rotation is fit for the phantom left hand accessory\nheadscale = 3,\nNetVelocity = Vector3.new(20,20,20), -- if your hands and head keep falling set these to higher numbers\ncontrollerRotationOffset = Vector3.new(90,90,0), -- movement offset\nHeadHatTransparency = 1,\nthirdPersonButtonToggle = nil, -- see what you look like (EXPERIMENTAL, set it to Enum.KeyCode.ButtonA if you want)\nleftToyBind = Enum.KeyCode.ButtonY, -- :D\nrightToyBind = Enum.KeyCode.ButtonB, -- :D\nleftToy = "'..configs.toyhats.leftarm..'", -- default is "" or nil\nrightToy = "'..configs.toyhats.rightarm..'", -- default is "" or nil\n}\ngetgenv().skyVRversion = \''..skyvrversion..'\'\nloadstring(game:HttpGet(\'https://raw.githubusercontent.com/loadlua/skyvr/main/SkyVR.lua\'))();'
+	
+		Export.Visible = true
+		Export.Script.Text = generatedScript
+		copy(generatedScript)
 	end)
-
-
+	
+	
 	ExtraSettings.Selection.lcfrot.FocusLost:Connect(function()
 		local split = string.split(ExtraSettings.Selection.lcfrot.Text,',')
 		if #split == 3 then
@@ -382,7 +385,7 @@ function fakescript()
 			ExtraSettings.Selection.lcfrot.Text = '0,0,0'
 		end
 	end)
-
+	
 	ExtraSettings.Selection.rcfrot.FocusLost:Connect(function()
 		local split = string.split(ExtraSettings.Selection.rcfrot.Text,',')
 		if #split == 3 then
@@ -392,20 +395,20 @@ function fakescript()
 			ExtraSettings.Selection.rcfrot.Text = '0,0,0'
 		end
 	end)
-
+	
 	game:GetService("RunService").RenderStepped:Connect(function()
 		local accessoriesActive = {}
-
+	
 		for i,v in pairs(global.skyvrsettings.headhats) do
 			if Preview:FindFirstChild(i) then 
 				local handleClone = Preview[i]
-
+	
 				handleClone.CFrame = Preview.head.CFrame * v
 				accessoriesActive[i]=1
 			else
 				local accessory = findMeshID(Character,i)
 				local handleClone = accessory.Handle:Clone()
-
+	
 				handleClone.Parent = Preview
 				handleClone.Name = i
 				handleClone.Anchored = true
@@ -413,20 +416,20 @@ function fakescript()
 				accessoriesActive[i]=1
 			end
 		end
-
+	
 		-- for anyone reading this, as a slight ego boost, i'm letting u know that i made leftarm check first :)
-
+	
 		if global.skyvrsettings.leftarm ~= "" then
 			local i = global.skyvrsettings.leftarm
 			if Preview:FindFirstChild(i) then 
 				local handleClone = Preview[i]
-
+	
 				handleClone.CFrame = Preview.larm.CFrame * CFrame.Angles(math.rad(larmcf.X+0),math.rad(larmcf.Y),math.rad(larmcf.Z))
 				accessoriesActive[i]=1
 			else
 				local accessory = findMeshID(Character,i)
 				local handleClone = accessory.Handle:Clone()
-
+	
 				handleClone.Parent = Preview
 				handleClone.Name = i
 				handleClone.Anchored = true
@@ -434,18 +437,18 @@ function fakescript()
 				accessoriesActive[i]=1
 			end
 		end
-
+	
 		if global.skyvrsettings.rightarm ~= "" then
 			local i = global.skyvrsettings.rightarm
 			if Preview:FindFirstChild(i) then 
 				local handleClone = Preview[i]
-
+	
 				handleClone.CFrame = Preview.rarm.CFrame * CFrame.Angles(math.rad(rarmcf.X+0),math.rad(rarmcf.Y),math.rad(rarmcf.Z))
 				accessoriesActive[i]=1
 			else
 				local accessory = findMeshID(Character,i)
 				local handleClone = accessory.Handle:Clone()
-
+	
 				handleClone.Parent = Preview
 				handleClone.Name = i
 				handleClone.Anchored = true
@@ -453,9 +456,9 @@ function fakescript()
 				accessoriesActive[i]=1
 			end
 		end
-
-
-
+	
+	
+	
 		for i,v in ipairs(Preview:GetChildren()) do
 			if v.Name:find('meshid:') then
 				if not ifind(accessoriesActive,v.Name) then
@@ -464,7 +467,7 @@ function fakescript()
 			end
 		end
 	end)
-
+	
 	updateList()
 end
 
