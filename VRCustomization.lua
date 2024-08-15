aa92876de282328ccf2159a1e47bcb4fd8943ca6
hadi <81989873+loadlua@users.noreply.github.com>
2024-08-14 20:02:42 -0400

    assetid change
    main account got terminated, so i switched out the id

--- VRCustomization.lua
+++ VRCustomization.lua
@@ -1,6 +1,6 @@
 skyvrversion = '2.0.0'
 
-VR_Model_Customization_GUI = game:GetObjects("rbxassetid://18596045046")[1]
+VR_Model_Customization_GUI = game:GetObjects("rbxassetid://18952348863")[1]
 VR_Model_Customization_GUI.Parent = game.CoreGui
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/loadlua/skyvr/main/VRCustomizationMain.lua"))()
